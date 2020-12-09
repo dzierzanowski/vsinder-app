0ff4edd5a670a396d5fb9ff37ad32802c1f966be
Waked <waked@incens.io>
2020-12-09 23:26:10 +0100

    Add gemfile and use bundler for fastlane

--- scripts/d_android.sh
+++ scripts/d_android.sh
@@ -8,4 +8,4 @@ npx expo build:android --non-interactive --type app-bundle
 
 curl -o app.aab $(expo url:apk --non-interactive)
 
-fastlane supply --track 'production' --json_key './api-copy.json' --package_name "com.benawad.vsinder" --aab "app.aab" --skip_upload_metadata --skip_upload_images --skip_upload_screenshots
+bundle exec fastlane supply --track 'production' --json_key './api-copy.json' --package_name "com.benawad.vsinder" --aab "app.aab" --skip_upload_metadata --skip_upload_images --skip_upload_screenshots
